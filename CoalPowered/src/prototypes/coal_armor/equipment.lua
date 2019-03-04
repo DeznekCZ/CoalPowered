56424d76926da50c314d44f888606b3acbfbe9ca
Phanteks <Phanteks@Phanteks-PC.lan>
2019-03-05 00:50:42 +0100

    Backup

--- CoalPowered/src/prototypes/coal_armor/equipment.lua
+++ CoalPowered/src/prototypes/coal_armor/equipment.lua
@@ -24,9 +24,9 @@ data:extend(
         {
           name = "smoke",
           deviation = { 0.1, 0.1 },
-          frequency = 5,
-          position = { 0.0, -0.8 },
-          starting_vertical_speed = 0.08,
+          frequency = 30,
+          position = { -0.2, 0.8 },
+          starting_vertical_speed = 0.04,
           starting_frame_deviation = 60
         }
       }
@@ -40,3 +40,92 @@ data:extend(
     categories = { "armor" }
   },
 })
+
+--[[ Recipes ]]
+
+with (data.raw.recipe["exoskeleton-equipment"], 
+  {
+    category = "advanced-crafting",
+    ingredients =
+    {
+      {"steel-plate", 20},
+      {"copper-cable", 10},
+      {"advanced-gear-box", 20}
+    },
+  }
+)
+
+with (data.raw.recipe["personal-roboport-equipment"], 
+  {
+    category = "advanced-crafting",
+    ingredients =
+    {
+      {"plastic-bar", 20},
+      {"advanced-gear-box", 10}
+    },
+  }
+)
+
+with (data.raw.recipe["personal-laser-defense-equipment"],
+  {
+    ingredients =
+    {
+      {"iron-gear-wheel", 5},
+      {"copper-plate", 5},
+      {"iron-plate", 10}
+    },
+  }
+)
+
+with (data.raw["active-defense-equipment"]["personal-laser-defense-equipment"],
+  {
+    energy_source =
+    {
+      type = "electric",
+      usage_priority = "secondary-input",
+      buffer_capacity = "0J"
+    },
+    attack_parameters =
+    {
+      type = "projectile",
+      ammo_category = "bullet",
+      cooldown = 12,
+      projectile_creation_distance = 1.39375,
+      projectile_center = {0, -0.0875}, -- same as gun_turret_attack shift
+      shell_particle =
+      {
+        name = "shell-particle",
+        direction_deviation = 0.1,
+        speed = 0.1,
+        speed_deviation = 0.03,
+        center = {-0.0625, 0},
+        creation_distance = -1.925,
+        starting_frame_speed = 0.2,
+        starting_frame_speed_deviation = 0.1
+      },
+      ammo_type = 
+      {
+        category = "bullet",
+        energy_consumption = "0J",
+        action = 
+        {
+          type = "direct",
+          action_delivery = 
+          {
+            type = "instant",
+            target_effects = {
+              type = "damage",
+              damage =
+              {
+                amount = 10,
+                type = "physical"
+              }
+            }
+          }
+        }
+      },
+      range = 18,
+      sound = make_heavy_gunshot_sounds()
+    },
+  }
+)
--- CoalPowered/src/prototypes/logistics.lua
+++ CoalPowered/src/prototypes/logistics.lua
@@ -68,31 +68,33 @@ with (data.raw.recipe["logistic-chest-storage"],
 --[[ Roboport ]]
 
 with (data.raw["roboport"]["roboport"].energy_source,
+{
+  type = "void",
+  fuel_category = "chemical",
+  effectivity = 1,
+  emissions = 0.02,
+  fuel_inventory_size = 1,
+  smoke =
   {
-    type = "void",
-    fuel_category = "chemical",
-    effectivity = 1,
-    emissions = 0.02,
-    fuel_inventory_size = 1,
-    smoke =
     {
-      {
-        name = "smoke",
-        frequency = 10,
-        position = {0.7, -1.2},
-        starting_vertical_speed = 0.08,
-        starting_frame_deviation = 60
-      }
-    },
-  }
-)
+      name = "smoke",
+      frequency = 10,
+      position = {0.7, -1.2},
+      starting_vertical_speed = 0.08,
+      starting_frame_deviation = 60
+    }
+  },
+})
+
+with (data.raw.item["roboport"], 
+{}, { "place_result" })
+data.raw["roboport"]["roboport"].order = data.raw.item["roboport"].order
 
 with (data.raw.recipe["roboport"], 
+{
+  ingredients =
   {
-    ingredients =
-    {
-      {"steel-plate", 45},
-      {"advanced-gear-box", 45}
-    },
-  }
-)
+    {"steel-plate", 45},
+    {"advanced-gear-box", 45}
+  },
+})
