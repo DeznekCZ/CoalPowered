8a6e23d0749d88d3b7748b63737019ac234ea0b4
Zdeněk Novotný <zde.nov.chr@seznam.cz>
2020-08-30 01:04:51 +0200

    Fix 0.0.2 and clearing

--- GunEquipment/src/prototypes/item.lua
+++ GunEquipment/src/prototypes/item.lua
@@ -2,16 +2,6 @@ data:extend(
 {
 	{
 		type = "item",
-		name = "portable-generator-equipment",
-		icon = "__GunEquipment__/graphics/icons/portable-generator-equipment.png",
-		icon_size = 32,
-		placed_as_equipment_result = "portable-generator-equipment",
-		subgroup = "equipment",
-		order = "a[energy-source]-b[portable-generator-equipment]",
-		stack_size = 10
-  },
-	{
-		type = "item",
 		name = "personal-turret-equipment",
 		localised_name = { "item-name.personal-turret-equipment" },
 		icon = "__GunEquipment__/graphics/icons/personal-turret-equipment.png",
